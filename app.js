13ecdfc4e99f7be0658e1d77ed897b10484669fe
srikanth Karra <srikanthreddy376@gmail.com>
2018-07-28 17:36:37 +0200

    added all require packages

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require("express"),
+app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
-app = express();
+passport =require("passport"),
+LocalStrategy = require("passport-local"),
+passportLocalMongoose = require("passport-local-mongoose"),
+User = require("./models/user");
+
 
 //CONNECT DB
-mongoose.connect("mongodb://localhost:27017/Auth_App");
+mongoose.connect("mongodb://localhost:27017/Auth_App", { useNewUrlParser: true });
+
+app.use(require("express-session")({
+   secret:"Iam eager learner",
+   resave:false,
+   saveUninitialized:false
+}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+app.use(passport.initialize());
+app.use(passport.session());
 
 //HOME ROUTE 
 app.get("/",function(req,res){
